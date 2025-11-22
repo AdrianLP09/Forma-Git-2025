a4acb6ae89de5b19daa2f535bb1b9a3f9e4c9dbe
Laura Brémont <laura.bremont@gmail.com>
2025-11-22 16:20:15 +0100

    modif par Laura et Rémi

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def is_over(buttons):
                 counter += 1
     # TODO: Corriger ce bug
     # Remplacer le return False par return counter == 0
-    return False
+    return counter == 0
     # return counter == 0
 
 
